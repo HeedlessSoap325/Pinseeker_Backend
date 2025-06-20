15bcc37e6349873f158fa74e5cb9e98c466bc27b
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-06-20 13:41:36 +0200

    Refactoring

--- src/main/java/de/heedlesssoap/pinseekerbackend/services/UserService.java
+++ src/main/java/de/heedlesssoap/pinseekerbackend/services/UserService.java
@@ -1,11 +1,9 @@
 package de.heedlesssoap.pinseekerbackend.services;
 
 import de.heedlesssoap.pinseekerbackend.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
