4707c7cc4683e5d3b6968443a1e7e59ad4d0d1f6
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-07-07 11:11:51 +0200

    Adapted Changes from DirectMessage.java;

--- src/main/java/de/heedlesssoap/pinseekerbackend/repositories/DirectMessageRepository.java
+++ src/main/java/de/heedlesssoap/pinseekerbackend/repositories/DirectMessageRepository.java
@@ -1,5 +1,6 @@
 package de.heedlesssoap.pinseekerbackend.repositories;
 
+import de.heedlesssoap.pinseekerbackend.entities.Chat;
 import de.heedlesssoap.pinseekerbackend.entities.DirectMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface DirectMessageRepository extends JpaRepository<DirectMessage, Integer> {
     @Query("SELECT dm FROM DirectMessage dm " +
-            "WHERE dm.chat_id = :chat_id AND dm.created_at = (" +
-            "SELECT MAX(dm2.created_at) FROM DirectMessage dm2 WHERE dm2.chat_id = :chat_id)")
-    Optional<DirectMessage> getLastMessageInChat(@Param("chat_id") Integer chat_id);
+            "WHERE dm.chat = :chat AND dm.created_at = (" +
+            "SELECT MAX(dm2.created_at) FROM DirectMessage dm2 WHERE dm2.chat = :chat)")
+    Optional<DirectMessage> getLastMessageInChat(@Param("chat_id") Chat chat);
 }
